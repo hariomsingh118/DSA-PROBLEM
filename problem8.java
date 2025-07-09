74d13e08a1ec90accf48511e239a54163c7ba28e
Hariom Singh <hariomsinghrajput236@gmail.com>
2025-07-09 20:54:21 +0530

    dsa problem 7 and 8

---
+++ problem8.java
@@ -0,0 +1,24 @@
+public class problem8 {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+
+        int result = findMaxConsecutiveOnes(nums);
+
+        System.out.println("Maximum consecutive 1s: " + result);
+    }
+}
